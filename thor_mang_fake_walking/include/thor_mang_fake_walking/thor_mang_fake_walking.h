b59e405adb53c48836de6ef10924652c023544a4
Paul Manns <pfmanns@gmail.com>
2015-06-01 01:10:45 +0200

    fake walking working with rosbag messages

--- thor_mang_fake_walking/include/thor_mang_fake_walking/thor_mang_fake_walking.h
+++ thor_mang_fake_walking/include/thor_mang_fake_walking/thor_mang_fake_walking.h
@@ -5,30 +5,36 @@
 #include <tf/transform_broadcaster.h>
 #include <gazebo_msgs/SetModelState.h>
 #include <actionlib/client/simple_action_client.h>
+#include <vigir_footstep_planning_msgs/ExecuteStepPlanActionGoal.h>
+#include <ros/console.h>
+
 
 namespace fake_walking{
 
 
 
-    class FakeWalking {
+class FakeWalking {
 
-    public:
-      FakeWalking(ros::NodeHandle& nh);
-      virtual ~FakeWalking();
+public:
+    FakeWalking(ros::NodeHandle& nh);
+    virtual ~FakeWalking();
 
-    protected:
-      void publishRobotPose(tf::Vector3 position, float yaw );
-       void setModelPose(double x, double y, double yaw);
+protected:
+    void publishRobotPose(tf::Vector3 position, float yaw );
+    void setModelPose(double x, double y, double yaw);
 
-
-
-    private:
-     ros::NodeHandle nh_;
-     tf::TransformBroadcaster tf_broadcaster;
-      ros::ServiceClient set_model_client_;
+    void executeFootstepCb(const vigir_footstep_planning_msgs::ExecuteStepPlanActionGoalConstPtr& goal);
 
 
-    };
+
+private:
+    ros::NodeHandle nh_;
+    tf::TransformBroadcaster tf_broadcaster;
+    ros::ServiceClient set_model_client_;
+    ros::Subscriber execute_footstep_sub_;
+
+
+};
 }
 
 #endif // THOR_MANG_FAKE_WALKING_H
--- thor_mang_fake_walking/src/thor_mang_fake_walking.cpp
+++ thor_mang_fake_walking/src/thor_mang_fake_walking.cpp
@@ -2,60 +2,110 @@
 #include <thor_mang_fake_walking/thor_mang_fake_walking.h>
 
 namespace fake_walking{
-    FakeWalking::FakeWalking(ros::NodeHandle &nh)
+
+FakeWalking::FakeWalking(ros::NodeHandle &nh)
+{
+    nh_ = nh;
+    set_model_client_ = nh_.serviceClient<gazebo_msgs::SetModelState>("/gazebo/set_model_state");
+    execute_footstep_sub_ = nh_.subscribe("/vigir/footstep_manager/execute_step_plan/goal", 10, &FakeWalking::executeFootstepCb, this);
+
+
+//    setModelPose(0.0, 0.0, 0.0);
+//    ros::Rate rate(ros::Duration(2.0));
+//    rate.sleep();
+//    setModelPose(0.2, 0, 0);
+//    rate.sleep();
+//    setModelPose(0.2, 0.5, 0);
+//    rate.sleep();
+//    setModelPose(0.2, 0.5, M_PI / 2);
+
+
+    // Next: Subscribe
+    //       /thor_mang/step_controller/execute_step_plan/goal
+}
+
+void FakeWalking::executeFootstepCb(const vigir_footstep_planning_msgs::ExecuteStepPlanActionGoalConstPtr& goal)
+{
+    ROS_INFO("FakeWalking::executeFootstepCb");
+
+    int n = goal->goal.step_plan.steps.size();
+    if(n < 2)
+        ROS_WARN("FakeWalking::executeFootstepCb not executed, need at least two steps.");
+
+    for(int i = 0; i < n; i += 2)
     {
-       nh_ = nh;
-       set_model_client_ = nh_.serviceClient<gazebo_msgs::SetModelState>("/gazebo/set_model_state");
+        vigir_footstep_planning_msgs::Step p1 = goal->goal.step_plan.steps[i];
+        vigir_footstep_planning_msgs::Step p2;
+        if(n > i + 1)
+            p2 = goal->goal.step_plan.steps[i + 1];
+        else
+        {
+            p2 = p1;
+            p1 = goal->goal.step_plan.steps[i - 1];
+        }
+        ros::Rate rate(ros::Duration(0.1));
+        rate.sleep();
 
-       setModelPose(0.0, 0.0, 0.0);
-      ros::Rate rate(ros::Duration(2.0));
-      rate.sleep();
-       setModelPose(0.2, 0, 0);
+        geometry_msgs::Pose pp1 = p1.foot.pose;
+        geometry_msgs::Pose pp2 = p2.foot.pose;
+        double x1 = pp1.position.x, y1 = pp1.position.y, x2 = pp2.position.x, y2 = pp2.position.y;
+        double x = (x1 + x2) / 2, y =  (y1 + y2) / 2;
 
-    }
+        tf::Quaternion q(pp1.orientation.x, pp1.orientation.y, pp1.orientation.z, pp1.orientation.w);
+        double roll, pitch, yaw;
+        tf::Matrix3x3(q).getRPY(roll, pitch, yaw);
 
-
-    FakeWalking::~FakeWalking()
-    {}
-
-
-    void FakeWalking::setModelPose(double x, double y, double yaw){
-        std::cout << " setting pose " << x << "," << y << ", " << yaw << std::endl;
-       set_model_client_.waitForExistence();
-      gazebo_msgs::SetModelState msg;
-            msg.request.model_state.model_name = "robot_description";
-
-            geometry_msgs::Pose pose;
-            tf::Quaternion q;
-            q.setRPY(0, 0, yaw);
-            pose.orientation.x=q.x();
-            pose.orientation.y=q.y();
-            pose.orientation.z=q.z();
-            pose.orientation.w=q.w();
-
-            pose.position.x = x;
-            pose.position.y = y;
-            pose.position.z = 0.86;
-            msg.request.model_state.pose = pose;
-
-            msg.request.model_state.reference_frame = "";
-
-            tf::Vector3 vec(x,y,0.0);
-
-            publishRobotPose(vec, yaw );
-
-
+        ROS_INFO("x = %f, y = %f, yaw = %f", x, y, yaw);
+        setModelPose(x, y, yaw);
 
     }
+}
 
-    void FakeWalking::publishRobotPose(tf::Vector3 position, float yaw ){
-          tf::Transform transform;
-          transform.setOrigin( position);
-          tf::Quaternion q;
-          q.setRPY(0, 0, yaw);
-          transform.setRotation(q);
-          tf_broadcaster.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", "pelvis"));
-    }
+
+
+FakeWalking::~FakeWalking()
+{}
+
+
+void FakeWalking::setModelPose(double x, double y, double yaw){
+    std::cout << " setting pose " << x << "," << y << ", " << yaw << std::endl;
+    set_model_client_.waitForExistence();
+    gazebo_msgs::SetModelState msg;
+    msg.request.model_state.model_name = "robot_description";
+
+    geometry_msgs::Pose pose;
+    tf::Quaternion q;
+    q.setRPY(0, 0, yaw);
+    pose.orientation.x=q.x();
+    pose.orientation.y=q.y();
+    pose.orientation.z=q.z();
+    pose.orientation.w=q.w();
+
+    pose.position.x = x;
+    pose.position.y = y;
+    pose.position.z = 0.86;
+    msg.request.model_state.pose = pose;
+
+    msg.request.model_state.reference_frame = "";
+
+    tf::Vector3 vec(x,y,0.0);
+
+    set_model_client_.call(msg);
+
+    publishRobotPose(vec, yaw );
+
+
+
+}
+
+void FakeWalking::publishRobotPose(tf::Vector3 position, float yaw ){
+    tf::Transform transform;
+    transform.setOrigin( position);
+    tf::Quaternion q;
+    q.setRPY(0, 0, yaw);
+    transform.setRotation(q);
+    tf_broadcaster.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", "pelvis"));
+}
 
 
 
