729d6dd9bbd5ea087046b702e6daed89fae84d9d
sternkopf <felix-sternkopf@web.de>
2018-09-06 15:36:14 +0200

    new package for virtual ft sensor reading

---
+++ thor_mang_virtual_ft_sensor/src/thor_mang_virtual_ft_sensor.cpp
@@ -0,0 +1,14 @@
+#include <thor_mang_virtual_ft_sensor/thor_mang_virtual_ft_sensor.h>
+
+namespace thormang3
+{
+VirtualFTSensor::VirtualFTSensor()
+{
+}
+
+VirtualFTSensor::~VirtualFTSensor()
+{
+}
+
+} // namespace
+
---
+++ thor_mang_virtual_ft_sensor/src/thor_mang_virtual_ft_sensor_node.cpp
@@ -0,0 +1,13 @@
+#include <thor_mang_virtual_ft_sensor/thor_mang_virtual_ft_sensor_node.h>
+
+namespace thormang3
+{
+VirtualFTSensorNode::VirtualFTSensorNode()
+{
+}
+
+VirtualFTSensorNode::~VirtualFTSensorNode()
+{
+}
+
+} // namespace
